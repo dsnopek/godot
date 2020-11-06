1343ec42a3e5091114b0e44ccb49cd7fcd7cfddf
David Snopek <dsnopek@gmail.com>
2020-11-06 11:25:11 -0600

    Remove failed attempt at fixing the compilation error

--- platform/javascript/engine/externs.js
+++ platform/javascript/engine/externs.js
@@ -1,17 +1,3 @@
 var Godot;
 var WebAssembly = {};
 WebAssembly.instantiate = function(buffer, imports) {};
-// This doesn't actually work.
-class XRWebGLLayer {
-    constructor(session, ctx) {
-        this.framebuffer;
-    }
-    getViewport(view) {}
-};
-// This doesn't work either.
-/*
-var XRWebGLLayer = function(session, ctx) {
-    this.framebuffer;
-};
-XRWebGLLayer.prototype.getViewport = function(view) {};
-*/

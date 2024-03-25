72e609062257525a47425579676ffceae235803b
David Snopek <dsnopek@gmail.com>
2024-03-25 08:04:36 -0500

    Fix fallback when running on the headset

--- modules/openxr/scene/openxr_composition_layer.cpp
+++ modules/openxr/scene/openxr_composition_layer.cpp
@@ -49,6 +49,7 @@ OpenXRCompositionLayer::OpenXRCompositionLayer() {
 	set_process_internal(true);
 
 	if (Engine::get_singleton()->is_editor_hint()) {
+		// In the editor, create the fallback right away.
 		_create_fallback_node();
 	}
 }
@@ -87,8 +88,12 @@ void OpenXRCompositionLayer::_create_fallback_node() {
 }
 
 void OpenXRCompositionLayer::_on_openxr_session_begun() {
-	if (!fallback && !is_natively_supported()) {
-		_create_fallback_node();
+	if (!is_natively_supported()) {
+		if (!fallback) {
+			_create_fallback_node();
+		}
+	} else if (layer_viewport && is_visible() && is_inside_tree()) {
+		openxr_layer_provider->set_viewport(layer_viewport);
 	}
 }
 
@@ -97,6 +102,8 @@ void OpenXRCompositionLayer::_on_openxr_session_stopping() {
 		fallback->queue_free();
 		remove_child(fallback);
 		fallback = nullptr;
+	} else {
+		openxr_layer_provider->set_viewport(nullptr);
 	}
 }
 
@@ -108,8 +115,8 @@ void OpenXRCompositionLayer::set_layer_viewport(SubViewport *p_viewport) {
 	layer_viewport = p_viewport;
 	if (fallback) {
 		_reset_fallback_material();
-	} else if (is_visible() && is_inside_tree()) {
-		openxr_layer_provider->set_viewport(p_viewport);
+	} else if (openxr_api && openxr_api->is_running() && is_visible() && is_inside_tree()) {
+		openxr_layer_provider->set_viewport(layer_viewport);
 	}
 }
 
@@ -137,6 +144,10 @@ bool OpenXRCompositionLayer::is_natively_supported() const {
 void OpenXRCompositionLayer::_reset_fallback_material() {
 	ERR_FAIL_NULL(fallback);
 
+	if (fallback->get_mesh().is_null()) {
+		return;
+	}
+
 	if (layer_viewport) {
 		Ref<StandardMaterial3D> material = fallback->get_surface_override_material(0);
 		if (material.is_null()) {
@@ -145,16 +156,18 @@ void OpenXRCompositionLayer::_reset_fallback_material() {
 			fallback->set_surface_override_material(0, material);
 		}
 
-		Node *loc_scene = material->get_local_scene();
-		if (loc_scene) {
-			Ref<ViewportTexture> texture = material->get_texture(StandardMaterial3D::TEXTURE_ALBEDO);
-			if (texture.is_null()) {
-				texture.instantiate();
-			}
-			NodePath viewport_path = loc_scene->get_path_to(layer_viewport);
-			texture->set_viewport_path_in_scene(viewport_path);
-			material->set_texture(StandardMaterial3D::TEXTURE_ALBEDO, texture);
+		Ref<ViewportTexture> texture = material->get_texture(StandardMaterial3D::TEXTURE_ALBEDO);
+		if (texture.is_null()) {
+			texture.instantiate();
+			// ViewportTexture can't be configured without a local scene, so use this hack to set it.
+			HashMap<Ref<Resource>, Ref<Resource>> remap_cache;
+			texture->configure_for_local_scene(this, remap_cache);
 		}
+
+		Node *loc_scene = texture->get_local_scene();
+		NodePath viewport_path = loc_scene->get_path_to(layer_viewport);
+		texture->set_viewport_path_in_scene(viewport_path);
+		material->set_texture(StandardMaterial3D::TEXTURE_ALBEDO, texture);
 	} else {
 		fallback->set_surface_override_material(0, Ref<Material>());
 	}
@@ -173,27 +186,25 @@ void OpenXRCompositionLayer::_notification(int p_what) {
 			}
 		} break;
 		case NOTIFICATION_VISIBILITY_CHANGED: {
-			if (!fallback && is_inside_tree()) {
+			if (!fallback && openxr_api && openxr_api->is_running() && is_inside_tree()) {
 				openxr_layer_provider->set_viewport(is_visible() ? layer_viewport : nullptr);
 			}
 		} break;
 		case NOTIFICATION_ENTER_TREE: {
-			if (!fallback) {
-				if (openxr_api) {
-					// Register our composition layer provider to our OpenXR API.
-					openxr_api->register_composition_layer_provider(openxr_layer_provider);
-				}
-				if (layer_viewport) {
-					openxr_layer_provider->set_viewport(is_visible() ? layer_viewport : nullptr);
-				}
+			if (openxr_api) {
+				// Register our composition layer provider to our OpenXR API.
+				openxr_api->register_composition_layer_provider(openxr_layer_provider);
+			}
+			if (!fallback && layer_viewport && openxr_api && openxr_api->is_running() && is_visible()) {
+				openxr_layer_provider->set_viewport(layer_viewport);
 			}
 		} break;
 		case NOTIFICATION_EXIT_TREE: {
+			if (openxr_api) {
+				// Unregister our composition layer provider.
+				openxr_api->unregister_composition_layer_provider(openxr_layer_provider);
+			}
 			if (!fallback) {
-				if (openxr_api) {
-					// Unregister our composition layer provider.
-					openxr_api->unregister_composition_layer_provider(openxr_layer_provider);
-				}
 				// This will clean up existing resources.
 				openxr_layer_provider->set_viewport(nullptr);
 			}

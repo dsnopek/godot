79872ff2d53b853b7d250bd75a077bd70af84ae5
David Snopek <dsnopek@gmail.com>
2026-02-10 13:20:49 -0600

    Better comment

--- drivers/vulkan/rendering_device_driver_vulkan.cpp
+++ drivers/vulkan/rendering_device_driver_vulkan.cpp
@@ -2045,9 +2045,8 @@ RDD::TextureID RenderingDeviceDriverVulkan::texture_create(const TextureFormat &
 		create_info.flags |= VK_IMAGE_CREATE_2D_ARRAY_COMPATIBLE_BIT;
 	}*/
 
-	// We check for `array_layers > 1` to avoid using subsampled images for 2D content (probably a Viewport).
+	// We only use this with 2D texture arrays with 2 layers to attempt limit it to stereoscopic rendering.
 	if (fdm_capabilities.attachment_supported && p_format.texture_type == TEXTURE_TYPE_2D_ARRAY && p_format.array_layers > 1 && (p_format.usage_bits & (TEXTURE_USAGE_COLOR_ATTACHMENT_BIT | TEXTURE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT | TEXTURE_USAGE_INPUT_ATTACHMENT_BIT | TEXTURE_USAGE_DEPTH_RESOLVE_ATTACHMENT_BIT))) {
-		//if (fdm_capabilities.attachment_supported && (p_format.usage_bits & (TEXTURE_USAGE_COLOR_ATTACHMENT_BIT | TEXTURE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT | TEXTURE_USAGE_INPUT_ATTACHMENT_BIT | TEXTURE_USAGE_DEPTH_RESOLVE_ATTACHMENT_BIT))) {
 		create_info.flags |= VK_IMAGE_CREATE_SUBSAMPLED_BIT_EXT;
 	}
 

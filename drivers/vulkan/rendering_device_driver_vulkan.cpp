c6dfcb0220d3f15d695190c0b7aa93339cfa5927
David Snopek <dsnopek@gmail.com>
2026-02-10 14:50:56 -0600

    Start adding setting for subsampled images

--- drivers/vulkan/rendering_device_driver_vulkan.cpp
+++ drivers/vulkan/rendering_device_driver_vulkan.cpp
@@ -2046,7 +2046,7 @@ RDD::TextureID RenderingDeviceDriverVulkan::texture_create(const TextureFormat &
 	}*/
 
 	// We only use this with 2D texture arrays with 2 layers to attempt limit it to stereoscopic rendering.
-	if (fdm_capabilities.attachment_supported && p_format.texture_type == TEXTURE_TYPE_2D_ARRAY && p_format.array_layers > 1 && (p_format.usage_bits & (TEXTURE_USAGE_COLOR_ATTACHMENT_BIT | TEXTURE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT | TEXTURE_USAGE_INPUT_ATTACHMENT_BIT | TEXTURE_USAGE_DEPTH_RESOLVE_ATTACHMENT_BIT))) {
+	if (fdm_capabilities.attachment_supported && p_format.texture_type == TEXTURE_TYPE_2D_ARRAY && p_format.array_layers > 1 && (p_format.usage_bits & (TEXTURE_USAGE_COLOR_ATTACHMENT_BIT | TEXTURE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT | TEXTURE_USAGE_INPUT_ATTACHMENT_BIT | TEXTURE_USAGE_DEPTH_RESOLVE_ATTACHMENT_BIT)) && (VulkanHooks::get_singleton() != nullptr && VulkanHooks::get_singleton()->use_subsampled_images())) {
 		create_info.flags |= VK_IMAGE_CREATE_SUBSAMPLED_BIT_EXT;
 	}
 
